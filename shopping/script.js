eac2897684319734d70d0e74592b0961eeb63741
Shayne Hewitt <shewitt565@gmail.com>
2025-06-28 16:14:03 -0400

    shopping page has items!

---
+++ shopping/script.js
@@ -0,0 +1,11 @@
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+const pages = {'meals':0, 'snacks':1, 'drinks':2};
+
+function MyApp(){}
+
+function MealsMenu(){}
+
+function SnacksMenu(){}
+
+function DrinksMenu(){}
